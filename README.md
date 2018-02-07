99de8d59e559f7805cc0757f6c13b3086d8d17bb
Ubuntu <ubuntu@ip-172-31-43-49.us-east-2.compute.internal>
2018-02-07 20:05:51 +0000

    upload the project online store to remote git repo

---
+++ README.md
@@ -0,0 +1,15 @@
+Please put hw3 files in this directory.
+
+This Homework implelment the interaction between client and the server using Django 1.11. Python 2.7.5
+It has the following funcitons:
+1.The server is stateles, the client sent the information needed for calculation with POST request.
+2.Used csrf_token to acoid being attacked
+3.Basicaly the same reaction as the last homework, but with calculation finished on server side.
+
+
+I refernce the following material:
+Django example provided by this course
+W3school
+Also, since I configure the .gitignore, it doesn't contain .pyc and other binary files, please run migrate before run my app.
+
+
---
+++ Store/views.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+# @csrf_exempt
+context={}
+class comment:
+	"""docstring for ClassName"""
+	name=""
+	content=""
+	def __init__(self, name, content):
+		self.name = name
+		self.content=content
+		
+comments=[];
+# Create your views here.
+def home_page(request):
+    # render takes: (1) the request,
+    #               (2) the name of the view to generate, and
+    #               (3) a dictionary of name-value pairs of data to be
+    #                   available to the view.
+
+    return render(request, 'home.html', {})
+
+def addcom(request):
+	print("in addcomm")
+	if 'username' in request.POST:
+		print(request.POST)
+		comm=[str(request.POST['username']),str(request.POST['comments'])]
+		comments.append(comm);
+		print(comments)
+		context={}
+		context['comments']=comments
+		return render(request, 'showcomm.html', context)
+		# print(comments[-1].name);
+		# print(comments[-1].content);
+	
+	
+	return render(request, 'addcomm.html',{})
+
+def addP(request):
+	
+	return render(request, 'addcomm.html', {})
+
+def showcom(request):
+
+	context={}
+	context['comments']=comments
+
+	return render(request, 'showcomm.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
